8743586037f409d2e57218dc7e0dfaee6a04fdc4
Woohyuk Song <harrywsong@naver.com>
2026-03-26 12:11:19 -0400

    Initial Commit

---
+++ SmartStudy/SmartStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartStudyApp.swift
+//  SmartStudy
+//
+//  Created by Woohyuk Song on 2026-03-26.
+//
+
+import SwiftUI
+
+@main
+struct SmartStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
